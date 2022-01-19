210bcfb2888a26a39542e1324b6fd797482b8b6a
Ervin Liiv <77278208+zzzervin@users.noreply.github.com>
2022-01-19 09:41:53 +0200

    Update Regist_and_Login.py

--- Regist_and_Login/Regist_and_Login.py
+++ Regist_and_Login/Regist_and_Login.py
@@ -1,16 +1,21 @@
 from module1 import *
 
-LNeme=["zzz"]
-NPass=["123"]
+LNeme=[]
+NPass=[]
 quest=["L","S","E"]
 
 while 1:
     ST=quest.index(input("(L)og_in, (S)ing_up, (E)xit >>>").upper())
     if ST==0:#Log_in
+        LNeme=Failis('Logins.txt',LNeme)
+        NPass=Failis('Pass.txt',NPass)
         Log(LNeme,NPass)
-        
+        print(LNeme)
     elif ST==1:#Sing_up
-        Sing(LNeme,NPass)        
+        LNeme=Failis('Logins.txt',LNeme)
+        NPass=Failis('Pass.txt',NPass)
+        Sing(LNeme,NPass)
+        rida_salvistamine('Logins.txt')        
         print(LNeme)
     elif ST==2:#Exit
         print("Good bey")
